7157590b3b4919855cf6c9fa3dce7f317dcf9248
RoyaAlmasimehr <almasimehrroya@gmail.com>
2023-12-09 15:46:46 +0330

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-     
-      </header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+
+        <footer className="App-footer">
+          This project was coded by Roya Almasimehr and is{" "}
+          <a href="https://github.com/RoyaAlmasimehr/dictionary-project">
+            open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a href="https://dictionary-project200.netlify.app/">
+            hosted on Netlify{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
